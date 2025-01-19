c7bb37043ed93d40b2d0c854a096cf2e2f6d59cd
DomBom16 <domenicjurso@gmail.com>
2025-01-19 14:38:13 -0500

    added citations routes

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const { openaiClient } = require("./config/openaiClient");
 const conversationRoutes = require("./routes/conversations");
 const fileRoutes = require("./routes/files");
 const chatRoutes = require("./routes/chat");
+const citationsRoutes = require("./routes/citations");
 const { cleanUpConversations } = require("./utils/cleanup");
 const readline = require("readline");
 
@@ -45,6 +46,7 @@ rl.on("line", (input) => {
 app.use("/api/conversation", conversationRoutes);
 app.use("/api/conversation", chatRoutes);
 app.use("/api/content", fileRoutes);
+app.use("/api/citations", citationsRoutes);
 
 // Serve the index.html file
 app.get("/", (req, res) => {
